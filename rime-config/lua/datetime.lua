1801784efc0a1874fcaaf70a68fb85bd9782c1c8
Lixp <golixp@outlook.com>
2022-10-23 02:52:40 +0800

    修改获取当前时间的lua脚本, 使其可以显示更多格式

--- rime-config/lua/datetime.lua
+++ rime-config/lua/datetime.lua
@@ -5,12 +5,17 @@
 local function translator(input, seg)
     if (input == "odt") then
         yield(Candidate("datetime", seg.start, seg._end, os.date("%Y-%m-%d %H:%M"), ""))
+        yield(Candidate("datetime", seg.start, seg._end, os.date("%Y.%m.%d %H:%M"), ""))
+        yield(Candidate("datetime", seg.start, seg._end, os.date("%Y/%m/%d %H:%M"), ""))
     end
     if (input == "od") then
-       yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y-%m-%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y.%m.%d"), ""))
+        yield(Candidate("date", seg.start, seg._end, os.date("%Y/%m/%d"), ""))
     end
     if (input == "ot") then
-       yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
+        yield(Candidate("time", seg.start, seg._end, os.date("%H:%M:%S"), ""))
+        yield(Candidate("time", seg.start, seg._end, os.date("%H:%M"), ""))
     end
 end
 
